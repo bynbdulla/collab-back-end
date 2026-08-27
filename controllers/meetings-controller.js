0057e9cca1233a42932516bbd81b8562469179d9
bynbdulla <b.markhoos@gmail.com>
2026-08-27 11:08:08 +0300

    meetings controller

---
+++ controllers/meetings-controller.js
@@ -0,0 +1,19 @@
+// const express = require("express")
+// const Meetings = require("../models/meetings")
+
+// const create = async (req,res) =>{
+//     try{
+//         req.body.author = req.user._id //attention
+//         const meeting = await Meetings.create(req.body)
+//         meeting._doc.author = req.user
+//         res.status(201).json(meeting)
+//     } catch(err){
+//         res.status(500).json({err: err.message})
+//     }
+// }
+
+
+
+// module.exports = {
+//     create,
+// }
