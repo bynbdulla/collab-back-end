b4ad5b6d80df27556f16d8fd60da3a861576e241
bynbdulla <b.markhoos@gmail.com>
2026-08-27 13:28:03 +0300

    workspace controller is done

--- controllers/workspace-controller.js
+++ controllers/workspace-controller.js
@@ -1,8 +1,79 @@
-const express =require("express")
-const Workspace = require("../models/workspace")
+const Workspace = require("../models/workspace.js")
 
+const create = async (req, res) => {
+  try {
+    req.body.owner = req.user._id
+    const workspace = await Workspace.create(req.body)
+    workspace._doc.owner = req.user
+    res.status(201).json(workspace)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
 
+const index = async (req, res) => {
+  try {
+    const workspaces = await Workspace.find({})
+      .populate("owner")
+      .sort({ createdAt: "desc" })
+    res.status(200).json(workspaces)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
+const show = async (req, res) => {
+  try {
+    const workspace = await Workspace.findById(req.params.workspaceId).populate([
+      'owner',
+    ])
+    res.status(200).json(workspace)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const workspace = await Workspace.findById(req.params.workspaceId)
+
+    if (!workspace.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not the admin!")
+    }
+
+    const updatedWorkspace = await Workspace.findByIdAndUpdate(
+      req.params.workspaceId,
+      req.body,
+      { new: true }
+    )
+
+    updatedWorkspace._doc.owner = req.user
+
+    res.status(200).json(updatedWorkspace)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
+const deleteWorkspace = async (req, res) => {
+  try {
+    const workspace = await Workspace.findById(req.params.workspaceId)
+
+    if (!workspace.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not the admin!")
+    }
+
+    const deletedWorkspace = await Workspace.findByIdAndDelete(req.params.workspaceId)
+    res.status(200).json(deletedWorkspace)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
 
 module.exports = {
-
+    create,
+    index,
+    show,
+    update,
+    deleteWorkspace,
 }
